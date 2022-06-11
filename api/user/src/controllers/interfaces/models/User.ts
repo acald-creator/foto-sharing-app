9df7578f79e65880d82b9b74c932b93409582f15
Antonette Caldwell <pullmana8@gmail.com>
2022-06-11 13:28:30 -0500

    Feature:
    Copied over some starter files onto user. Added some more packages.

---
+++ api/user/src/controllers/interfaces/models/User.ts
@@ -0,0 +1,12 @@
+class User {
+    public email!: string;
+    public password_hash!: string;
+    public createdAt: Date = new Date();
+    public updatedAt: Date = new Date();
+
+    short() {
+        return {
+            email: this.email
+        }
+    }
+}
