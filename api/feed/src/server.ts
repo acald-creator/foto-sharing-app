378282c6d1818882016ae28d13978167349c6545
Antonette Caldwell <pullmana8@gmail.com>
2022-06-11 11:05:27 -0500

    Added `aws-sdk` and set up on to obtained signed url

--- api/feed/src/server.ts
+++ api/feed/src/server.ts
@@ -1,4 +1,5 @@
 import HTTP from 'http'
+import * as AWS from 'aws-sdk'
 
 const config = {
     "dev": {
@@ -25,6 +26,50 @@ const config = {
 }
 
 const c = config.dev
+
+const credentials = new AWS.SharedIniFileCredentials({ profile: c.aws_profile })
+AWS.config.credentials = credentials
+
+const s3 = new AWS.S3({
+    signatureVersion: 'v4',
+    region: c.aws_region,
+    params: { Bucket: c.aws_media_bucket},
+})
+
+const signedURLExpireSeconds = 60 * 5
+
+/* getGetSignedUrl generates an aws signed url to retreive an item
+ * @Params
+ *    key: string - the filename to be put into the s3 bucket
+ * @Returns:
+ *    a url as a string
+ */
+function getSigedURL(key: string): string {
+    const url = s3.getSignedUrl('getObject', {
+        Bucket: c.aws_media_bucket,
+        Key: key,
+        Expires: signedURLExpireSeconds,
+    })
+
+    return url
+}
+
+/* getPutSignedUrl generates an aws signed url to put an item
+ * @Params
+ *    key: string - the filename to be retreived from s3 bucket
+ * @Returns:
+ *    a url as a string
+ */
+function getPutSignedURL(key: string) {
+    const url = s3.getSignedUrl('putObject', {
+        Bucket: c.aws_media_bucket,
+        Key: key,
+        Expires: signedURLExpireSeconds
+    })
+
+    return url
+}
+
 const port = process.env.PORT || 8080
 const newServer = HTTP.createServer((req, res) => {
     res.write('Hello!')
@@ -32,7 +77,7 @@ const newServer = HTTP.createServer((req, res) => {
 })
 
 newServer.listen(port, () => {
-    console.log(`server running  at http://localhost:${port}/`) // @TODO c.url
+    console.log(`server running at http://localhost:${port}/`) // @TODO c.url
     console.log(`press CTRL+C to stop server`)
 })
 
