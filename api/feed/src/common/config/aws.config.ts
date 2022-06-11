8397dff3ee8586bfd97c8c05acd134673f20064a
Antonette Caldwell <pullmana8@gmail.com>
2022-06-11 14:25:36 -0500

    Feature: Updated feed rest api
    Removed yarn again, going back to just using npm
    Moved aws configuration and database configuration to its files
    Setup sequelize

---
+++ api/feed/src/common/config/aws.config.ts
@@ -0,0 +1,47 @@
+import * as AWS from 'aws-sdk'
+import { config } from './database.config'
+
+const c = config.dev
+
+const credentials = new AWS.SharedIniFileCredentials({ profile: c.aws_profile })
+AWS.config.credentials = credentials
+
+const s3 = new AWS.S3({
+    signatureVersion: 'v4',
+    region: c.aws_region,
+    params: { Bucket: c.aws_media_bucket},
+})
+
+const signedURLExpireSeconds = 60 * 5
+
+/* getGetSignedUrl generates an aws signed url to retreive an item
+ * @Params
+ *    key: string - the filename to be put into the s3 bucket
+ * @Returns:
+ *    a url as a string
+ */
+function getSigedURL(key: string): string {
+    const url = s3.getSignedUrl('getObject', {
+        Bucket: c.aws_media_bucket,
+        Key: key,
+        Expires: signedURLExpireSeconds,
+    })
+
+    return url
+}
+
+/* getPutSignedUrl generates an aws signed url to put an item
+ * @Params
+ *    key: string - the filename to be retreived from s3 bucket
+ * @Returns:
+ *    a url as a string
+ */
+function getPutSignedURL(key: string) {
+    const url = s3.getSignedUrl('putObject', {
+        Bucket: c.aws_media_bucket,
+        Key: key,
+        Expires: signedURLExpireSeconds
+    })
+
+    return url
+}
--- api/feed/src/server.ts
+++ api/feed/src/server.ts
@@ -2,81 +2,11 @@ import cors from 'cors'
 import HTTP from 'http'
 import debug from 'debug'
 import express from 'express'
-import * as AWS from 'aws-sdk'
 import * as winston from 'winston'
 import * as expressWinston from 'express-winston'
-import { CommonRoutesConfig } from './common/common.routes.config'
-import { FeedRoutes } from './routes/feed.routes.config'
 import { IndexRoutes } from './routes/index.router'
-
-const config = {
-    "dev": {
-        "username": process.env.POSTGRES_USERNAME,
-        "password": process.env.POSTGRES_PASSWORD,
-        "database": process.env.POSTGRES_DB,
-        "host": process.env.POSTGRES_HOST,
-        "dialect": "postgres",
-        "aws_region": process.env.AWS_REGION,
-        "aws_profile": process.env.AWS_PROFILE,
-        "aws_media_bucket": process.env.AWS_BUCKET,
-        "url": process.env.URL
-    },
-    "prod": {
-        "username": "",
-        "password": "",
-        "database": "foto-share-prod",
-        "host": "",
-        "dialect": "postgres"
-    },
-    "jwt": {
-        "secret": process.env.JWT_SECRET
-    }
-}
-
-const c = config.dev
-
-const credentials = new AWS.SharedIniFileCredentials({ profile: c.aws_profile })
-AWS.config.credentials = credentials
-
-const s3 = new AWS.S3({
-    signatureVersion: 'v4',
-    region: c.aws_region,
-    params: { Bucket: c.aws_media_bucket},
-})
-
-const signedURLExpireSeconds = 60 * 5
-
-/* getGetSignedUrl generates an aws signed url to retreive an item
- * @Params
- *    key: string - the filename to be put into the s3 bucket
- * @Returns:
- *    a url as a string
- */
-function getSigedURL(key: string): string {
-    const url = s3.getSignedUrl('getObject', {
-        Bucket: c.aws_media_bucket,
-        Key: key,
-        Expires: signedURLExpireSeconds,
-    })
-
-    return url
-}
-
-/* getPutSignedUrl generates an aws signed url to put an item
- * @Params
- *    key: string - the filename to be retreived from s3 bucket
- * @Returns:
- *    a url as a string
- */
-function getPutSignedURL(key: string) {
-    const url = s3.getSignedUrl('putObject', {
-        Bucket: c.aws_media_bucket,
-        Key: key,
-        Expires: signedURLExpireSeconds
-    })
-
-    return url
-}
+import { FeedRoutes } from './routes/feed.routes.config'
+import { CommonRoutesConfig } from './common/common.routes.config'
 
 const app: express.Application = express()
 const port = process.env.PORT || 8080
