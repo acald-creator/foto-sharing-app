8d5f0e9187cb00576fb5d2f008a928afdd72cd86
Antonette Caldwell <pullmana8@gmail.com>
2022-06-11 21:35:58 -0500

    (feat) rearranged the server.ts to better initialize the app

--- api/user/src/server.ts
+++ api/user/src/server.ts
@@ -7,41 +7,48 @@ import { IndexRoutes } from './routes/index.router'
 import { UserRoutes } from './routes/user.routes.config'
 import { CommonRoutesConfig } from './common/common.routes.config'
 
-const port = process.env.PORT || 8080
-const app: express.Application = express()
-const routes: Array<CommonRoutesConfig> = []
-const debugLog: debug.IDebugger = debug('app')
+export function InitExpressApp(port = process.env.PORT || 8080): void {
+    const app: express.Application = express()
+    const routes: Array<CommonRoutesConfig> = []
+    const debugLog: debug.IDebugger = debug('app')
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors())
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.json())
+    app.use(cors())
 
-const loggerOptions: expressWinston.LoggerOptions = {
-    transports: [new winston.transports.Console()],
-    format: winston.format.combine(
-        winston.format.json(),
-        winston.format.prettyPrint(),
-        winston.format.colorize({ all: true }),
-    ),
-}
+    const loggerOptions: expressWinston.LoggerOptions = {
+        transports: [new winston.transports.Console()],
+        format: winston.format.combine(
+            winston.format.json(),
+            winston.format.prettyPrint(),
+            winston.format.colorize({ all: true }),
+        ),
+    }
 
-if (!process.env.DEBUG) {
-    loggerOptions.meta = false
-}
+    if (!process.env.DEBUG) {
+        loggerOptions.meta = false
+    }
 
-app.use(expressWinston.logger(loggerOptions))
+    app.use(expressWinston.logger(loggerOptions))
 
-routes.push(new IndexRoutes(app))
-routes.push(new UserRoutes(app))
+    routes.push(new IndexRoutes(app))
+    routes.push(new UserRoutes(app))
 
-const runningMessage = `Server running at http://localhost:${port}`
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.status(200).send(runningMessage)
-})
-
-app.listen(port, () => {
-    routes.forEach((route: CommonRoutesConfig) => {
-        debugLog(`Routes configured for ${route.getName()}`)
+    const runningMessage = `Server running at http://localhost:${port}`
+    app.get('/', function (req: express.Request, res: express.Response) {
+        res.status(200).send(runningMessage)
     })
-    console.log(runningMessage)
-})
+
+    app.use(function (req: express.Request, res: express.Response) {
+        res.status(404).send('Not found')
+    })
+
+    app.listen(port, () => {
+        routes.forEach((route: CommonRoutesConfig) => {
+            debugLog(`Routes configured for ${route.getName()}`)
+        })
+        console.log(runningMessage)
+    })
+}
+
+InitExpressApp()
